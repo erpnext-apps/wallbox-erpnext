bf2f60d034535f935791cd17ad3bb6244652c44d
Ranjith <rkurungadam@gmail.com>
2018-09-19 20:02:00 +0530

    Lead Time Documents

---
+++ wallbox_erpnext/quality/doctype/wallbox_manufacturing_lead_time/wallbox_manufacturing_lead_time.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2018, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Wallbox Manufacturing Lead Time', {
+	setup: function(frm) {
+		frm.set_query("item_group", function(){
+			return {
+				filters : {
+					"is_group": 0
+				}
+			}
+		});
+	}
+});

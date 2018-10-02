276c4285ab2bce16f2ff8ed9358f5764f94a0146
Ranjith <rkurungadam@gmail.com>
2018-10-02 12:31:44 +0530

    LT by item group, calc on total item qty in group

--- wallbox_erpnext/quality/utils.py
+++ wallbox_erpnext/quality/utils.py
@@ -121,7 +121,7 @@ def get_data_by_item_group(doc):
 			item_group_lt[item.item_group] = {}
 			item_group_lt[item.item_group]["qty"] = item.qty
 		elif item_group_lt[item.item_group]["qty"] < item.qty:
-			item_group_lt[item.item_group]["qty"] = item.qty
+			item_group_lt[item.item_group]["qty"] += item.qty
 		if frappe.db.get_value("Item", item.item_code, "wb_is_not_standard"):
 			item_group_lt[item.item_group]["has_custom"] = 1
 	return item_group_lt
